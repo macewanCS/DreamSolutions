5d12943e7f094ba0206adefefe5510c3a8c8dfa6
reboss <mulvanyrobbinsj@gmail.com>
2016-02-15 16:16:45 -0700

    Link header to routes and css file

--- src/epl-business-plan-manager/resources/views/main_header.blade.php
+++ src/epl-business-plan-manager/resources/views/main_header.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Business Plan Manager</title>
-    <link rel="stylesheet" type="text/css" href="/css/header1.css">
+    <link rel="stylesheet" type="text/css" href="/css/header.css">
 </head>
 <body>
     <div id="header-area">
