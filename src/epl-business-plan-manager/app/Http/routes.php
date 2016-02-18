320eca64f66321b3f89e6a5c65e8306297204c15
reboss <mulvanyrobbinsj@gmail.com>
2016-02-17 17:23:17 -0700

    Route to dashboard using DashboardController

--- src/epl-business-plan-manager/app/Http/routes.php
+++ src/epl-business-plan-manager/app/Http/routes.php
@@ -20,9 +20,7 @@ Route::get('login', function () {
     return view('login');
 });
 
-Route::get('dashboard', function () {
-    return view('main_header');
-});
+Route::get('dashboard', 'DashboardController@dashboard');
 
 
 /*
