0d372de783d495ef3fb5a655f95e62d7c42cc9c0
Elliot Sobek <elliottsobek@gmail.com>
2016-02-18 20:11:50 -0700

    add manage plan file with some boiler plate

---
+++ src/epl-business-plan-manager/resources/views/manage_plan.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"></meta>
+    <title>Business Plan Manager</title>
+    <link rel="stylesheet" type="text/css" href="/css/header.css"></link>
+    <link rel="stylesheet" type="text/css" href="/css/managePlan.css"></link>
+</head>
+<body>
+    <div id="header-area">
+        <header id="main-header">
+            <img id="epl-logo" src="images/epl-logo.jpg" alt="EPL Logo">
+            <h3>Business Plan Manager</h3>
+        </header>   
+        <hr>
+        <div id="nav">
+            <ul>
+                <li><a href="#dashboard">Dashboard</a></li>
+                <li><a href="#view_plan" class="active">View Plan</a></li>
+                <li><a href="#manage_plan">Manage Plan</a></li>
+            </ul>
+            <div>
+            	<img style="float: right;" src="https://static.xx.fbcdn.net/rsrc.php/v2/yB/r/Unmn04Ngmxd.gif">
+            	<input type="text" name="search" placeholder="Search" style="float: right;">
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
