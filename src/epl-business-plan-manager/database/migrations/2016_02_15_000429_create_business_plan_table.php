74c61133b01aec1c57676ac7e4d4ec68331a771e
Cody Moorhouse <moorhousec2@mymacewan.ca>
2016-02-19 15:12:20 -0700

    Remove merge conflict notice in code

--- src/epl-business-plan-manager/database/migrations/2016_02_15_000429_create_business_plan_table.php
+++ src/epl-business-plan-manager/database/migrations/2016_02_15_000429_create_business_plan_table.php
@@ -12,13 +12,8 @@ class CreateBusinessPlanTable extends Migration
      */
     public function up()
     {
-<<<<<<< 0a97bbc50ab43c122f3d1f57fcbbed20cbc1d8d8:src/epl-business-plan-manager/database/migrations/2016_02_15_000429_create_business_plan_table.php
-        Schema::create('business_plan', function (Blueprint $table) {
-            $table->increments('id');
-=======
         Schema::create('business_plans', function (Blueprint $table) {
             $table->increments('bid');
->>>>>>> Add dashboard view, Make migration tables plural:src/epl-business-plan-manager/database/migrations/2016_02_14_000002_create_business_plan_table.php
             $table->date('start');
             $table->date('end');
             $table->timestamps();
