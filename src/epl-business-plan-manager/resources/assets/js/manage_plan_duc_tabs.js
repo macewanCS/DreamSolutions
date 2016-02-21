88f0dbf1550a9176ab910d455e7800eabac24fe7
Elliot Sobek <elliottsobek@gmail.com>
2016-02-20 23:35:30 -0700

    add error documentation

--- src/epl-business-plan-manager/resources/assets/js/manage_plan_duc_tabs.js
+++ src/epl-business-plan-manager/resources/assets/js/manage_plan_duc_tabs.js
@@ -11,3 +11,41 @@ jQuery(document).ready(function() {
         e.preventDefault();
     });
 });
+
+
+<!-- // <script type="text/javascript" src="http://code.jquery.com/jquery-1.12.0.js"></script> -->
+<script type="text/javascript">
+            $(function() {
+
+                $('.duc-tabs .duc-tab-links li').on('click', function() {
+
+                    var $tab = $(this).closest('.duc-tabs');
+
+                    $tab.find('.duc-tab-links li.active').removeClass('active');
+
+                    $(this).addClass('active');
+
+                    // figure out which tab to show
+                    var tabToShow = $(this).attr('href'); // Problem is here, undefined.
+                    alert(tabToShow);
+
+                    // hide current panel
+                    $tab.find('.tab.active').hide(showNewTab);
+
+                    function showNewTab() {
+                        // Remove active class
+                        $(this).removeClass('active');
+                        alert('Inmore');
+
+                        // Show current pannel
+                        $('#'+tabToShow).show(function() {
+
+                        // add active class
+                        $(this).addClass('active');
+                        alert('End');
+                        });
+                    }
+
+                });
+            })
+        </script>
