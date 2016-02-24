048520b977b756eb72b5d78176a897eecd68db76
Elliot Sobek <elliottsobek@gmail.com>
2016-02-24 15:19:13 -0700

    add 'head' section & remove stylesheets

--- src/epl-business-plan-manager/resources/views/app.blade.php
+++ src/epl-business-plan-manager/resources/views/app.blade.php
@@ -3,14 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Business Plan Manager</title>
-    <link rel="stylesheet" type="text/css" href="/css/header.css">
-    <link rel="stylesheet" type="text/css" href="/css/dashboard.css">
+	@yield('head')
 </head>
 <body>
 
 	@include('main_header')
 
 	@yield('content')
-	
+
 </body>
 </html>
