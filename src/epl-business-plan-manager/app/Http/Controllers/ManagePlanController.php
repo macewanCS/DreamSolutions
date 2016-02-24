973fcc0eaa36417b8c12b5b6aee1dc8ddecb6d06
Elliot Sobek <elliottsobek@gmail.com>
2016-02-24 15:16:31 -0700

    change method names to match resource convention

--- src/epl-business-plan-manager/app/Http/Controllers/ManagePlanController.php
+++ src/epl-business-plan-manager/app/Http/Controllers/ManagePlanController.php
@@ -3,26 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class ManagePlanController extends Controller
 {
-    public function store() {
 
-    }
-
-    public function edit() {
-
-    }
-
-    public function delete() {
-
-    }
-
-    public function home() {
+    public function index()
+    {
         return view('manage_plan');
     }
 
+    public function store()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function destroy()
+    {
+    }
 }
