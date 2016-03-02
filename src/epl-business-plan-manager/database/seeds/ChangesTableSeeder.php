21c6fef57ffd5d6b59ee84eed199d9ab3bb929b8
Cody Moorhouse <moorhousec2@mymacewan.ca>
2016-03-02 11:09:37 -0700

    Add Change Table Seeder Functionality

--- src/epl-business-plan-manager/database/seeds/ChangesTableSeeder.php
+++ src/epl-business-plan-manager/database/seeds/ChangesTableSeeder.php
@@ -13,36 +13,50 @@ class ChangesTableSeeder extends Seeder
     {
         DB::table('changes')->insert(array(
 
+            // 1
+            // Vicky Varga
+            // Review public computing needs and develop strategies to meet those needs.
             array(
-                'change_type' => ,
-                'description' => ,
-                'goat_id' => ,
-                'user_id' => ),
+                'change_type' => 'N',
+                'description' => '10 locations now have making tech and iMacs roll out shortly.',
+                'goat_id' => 13,
+                'user_id' => 1),
 
+            // 2
+            // Vicky Varga
+            // Implement approved recommendations from the 2015 Public Computing Report
             array(
-                'change_type' => ,
-                'description' => ,
-                'goat_id' => ,
-                'user_id' => ),
+                'change_type' => 'S',
+                'description' => 'Projects in each of 2014, 2015, 2016.',
+                'goat_id' => 14,
+                'user_id' => 1),
 
+            // 3
+            // John Doe
+            // Live stream two forward thinking speaker series events in 2016
             array(
-                'change_type' => ,
-                'description' => ,
-                'goat_id' => ,
-                'user_id' => ),
+                'change_type' => 'N',
+                'description' => 'Stanley said to talk with Billy who has some great ideas',
+                'goat_id' => 11,
+                'user_id' => 2),
 
+            // 4
+            // Vicky Varga
+            // Evaluate the 2016 event and create a proposal for the 2017 by November 30, 2016.
             array(
-                'change_type' => ,
-                'description' => ,
+                'change_type' => 'S',
+                'description' => 'Completed',
+                'goat_id' => 10,
+                'user_id' => 1),
 
-                'goat_id' => ,
-                'user_id' => ),
-
+            // 5
+            // Mark Smith
+            // Host EPL Day celebrations at all branches on March 13, 2016
             array(
-                'change_type' => ,
-                'description' => ,
-                'goat_id' => ,
-                'user_id' => )
+                'change_type' => 'N',
+                'description' => 'Talked to John Doe who told me something useful.',
+                'goat_id' => 9,
+                'user_id' => 3)
 
         ));
     }
