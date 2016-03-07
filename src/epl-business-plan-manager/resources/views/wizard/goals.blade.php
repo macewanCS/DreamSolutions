ed070ee59efb8a0256e8b9d95b88a0ba2c6c5367
Cody Moorhouse <moorhousec2@mymacewan.ca>
2016-03-06 23:44:42 -0700

    Add '+' button onto goal screen

--- src/epl-business-plan-manager/resources/views/wizard/goals.blade.php
+++ src/epl-business-plan-manager/resources/views/wizard/goals.blade.php
@@ -12,13 +12,26 @@
         <h1 id="wizard-goal-title">Enter Goals</h1>
 
         <div class="text-boxes">
+
+
             <?php $numGoals = 4; ?>
             @for ($i = 1; $i < $numGoals; $i++)
-                <div class="goal-area">
-                    {!! Form::label('Goal '.$i.': ', '', array('class' => 'goal-text-label')) !!}
-                    {!! Form::textarea('Goal '.$i, '', array('class' => 'goal-text-box')) !!}
-                </div>
+                @if (($i + 1) != $numGoals)
+                    <div class="goal-area">
+                        {!! Form::label('Goal '.$i.': ', '', array('class' => 'goal-text-label')) !!}
+                        {!! Form::textarea('Goal '.$i, '', array('class' => 'goal-text-box')) !!}
+                    </div>
+                @else
+                    <div class="last-goal-area">
+                        {!! Form::label('Goal '.$i.': ', '', array('class' => 'goal-text-label')) !!}
+                        {!! Form::textarea('Goal '.$i, '', array('class' => 'goal-text-box')) !!}
+                        {!! Form::button('+', array('id' => 'add-goal-action')) !!}
+                    </div>
+                @endif
             @endfor
+
+        </div>
+
 
         <div class="wizard-button-area-goals">
             @include ('wizard.partials.buttons', ['nextButtonAction' => 'WizardController@createGoals'])
