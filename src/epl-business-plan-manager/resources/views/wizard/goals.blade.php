7f2c8ddee1059de692e10c6a1ca45e9580aea9f0
Cody Moorhouse <moorhousec2@mymacewan.ca>
2016-03-06 23:20:13 -0700

    Convert goal form to for loop

--- src/epl-business-plan-manager/resources/views/wizard/goals.blade.php
+++ src/epl-business-plan-manager/resources/views/wizard/goals.blade.php
@@ -9,26 +9,16 @@
 
     <div id="wizard-goal-selection">
 
-        <h1 id="wizard-goal-title">Select Goals</h1>
+        <h1 id="wizard-goal-title">Enter Goals</h1>
 
         <div class="text-boxes">
-
-            <div class="goal-area">
-                {!! Form::label('Goal 1: ', '', array('class' => 'goal-text-label')) !!}
-                {!! Form::textarea('Goal 1', '', array('class' => 'goal-text-box')) !!}
-            </div>
-
-            <div class="goal-area">
-                {!! Form::label('Goal 2: ', '', array('class' => 'goal-text-label')) !!}
-                {!! Form::textarea('Goal 2', '', array('class' => 'goal-text-box')) !!}
-            </div>
-
-            <div class="goal-area">
-                {!! Form::label('Goal 3: ', '', array('class' => 'goal-text-label')) !!}
-                {!! Form::textarea('Goal 3', '', array('class' => 'goal-text-box')) !!}
-            </div>
-
-        </div>
+            <?php $numGoals = 4; ?>
+            @for ($i = 1; $i < $numGoals; $i++)
+                <div class="goal-area">
+                    {!! Form::label('Goal '.$i.': ', '', array('class' => 'goal-text-label')) !!}
+                    {!! Form::textarea('Goal '.$i, '', array('class' => 'goal-text-box')) !!}
+                </div>
+            @endfor
 
         <div class="wizard-button-area-goals">
             @include ('wizard.partials.buttons', ['nextButtonAction' => 'WizardController@createGoals'])
