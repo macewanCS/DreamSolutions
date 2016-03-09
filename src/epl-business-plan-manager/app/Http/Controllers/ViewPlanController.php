3148d657f9bb73e0c7e6f80a6f2945f452820905
Alan Yong <yonga@mymacewan.ca>
2016-03-08 17:53:46 -0700

    Fix bug where sorted BP only contained goals in Linux

--- src/epl-business-plan-manager/app/Http/Controllers/ViewPlanController.php
+++ src/epl-business-plan-manager/app/Http/Controllers/ViewPlanController.php
@@ -10,27 +10,28 @@ use App\Http\Controllers\Controller;
 class ViewPlanController extends Controller
 {
     public function index() {
-    	// TODO: CACHE THIS!!
-    	$bp = Goat::all();
+        // TODO: CACHE THIS!!
+        $bp = Goat::all();
 
-    	$sorted = collect();
-    	foreach ($bp as $goat) {
-    		if ($goat->type === 'G') {
-    			$sorted->push($goat);
-    			continue;
-    		}
+        $sorted = collect();
+        foreach ($bp as $goat) {
+            if ($goat->type === 'G') {
+                $sorted->push($goat);
+                continue;
+            }
 
-    		for ($i = 0, $len = $sorted->count(); $i < $len; $i++) {
-    			if ($sorted[$i]->id === $goat->parent_id) {
-    				// Hacky fix since when you splice $goat into $sorted, it converts the $goat into
-    				// an array instead of keeping it as a Model object...
-    				$sorted->splice($i + 1, 0, "temp");
-    				$sorted->put($i + 1, $goat);
-    				break;
-    			}
-    		}
-    	}
+            for ($i = 0, $len = $sorted->count(); $i < $len; $i++) {
+                if ($sorted[$i]->id == $goat->parent_id) {
+                    // Hacky fix since when you splice $goat into $sorted, 
+                    // it converts the $goat into an array instead of 
+                    // keeping it as a Model object...
+                    $sorted->splice($i + 1, 0, "temp");
+                    $sorted->put($i + 1, $goat);
+                    break;
+                }
+            }
+        }
 
-    	return view('view_plan')->with('bp', $sorted);
+        return view('view_plan')->with('bp', $sorted);
     }
 }
