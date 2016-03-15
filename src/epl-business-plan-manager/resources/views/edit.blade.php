b113603250db2fc9f9ea2afe81af68f47ac8864c
reboss <mulvanyrobbinsj@gmail.com>
2016-03-15 10:41:28 -0600

    Modify edit page

--- src/epl-business-plan-manager/resources/views/edit.blade.php
+++ src/epl-business-plan-manager/resources/views/edit.blade.php
@@ -22,10 +22,29 @@
 		</div>
 
 		<div id="right">
-
+			<table>
+				<thead>
+					<tr>
+						<th>Date</th>
+						<th>Author</th>
+						<th>Type</th>
+						<th>Update</th>
+					</tr>
+				</thead>
+				<tbody>
+					
+				</tbody>
+			</table>
 		</div>
 
 		<div id="bottom">
+			 	{!! Form::open() !!}
+                {!! Form::label('Enter Status Update:', null, ['class' => 'label']) !!}<br>
+                {!! Form::textarea('statusUpdate', null, ['class' => 'text-area']) !!}<br>
+                {!! Form::checkbox('agree', 1, null, ['class' => 'field']) !!}
+                {!! Form::label('Task Complete', null, ['class' => 'clabel']) !!}
+                {!! Form::submit('submit', ['class' => 'button']) !!}
+                {!! Form::close() !!}
 		</div>
 
 	</div>
