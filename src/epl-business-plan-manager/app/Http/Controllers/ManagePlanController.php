9c1f043380268c5cd26686ab3664c348096f8a17
Elliot Sobek <elliottsobek@gmail.com>
2016-03-29 11:58:10 -0600

    implement issue 54

--- src/epl-business-plan-manager/app/Http/Controllers/ManagePlanController.php
+++ src/epl-business-plan-manager/app/Http/Controllers/ManagePlanController.php
@@ -14,12 +14,6 @@ use Illuminate\Support\Facades\Input;
 
 class ManagePlanController extends Controller
 {
-
-    // public function __construct()
-    //    {
-    //        $this->middleware('auth');
-    //    }
-
     public function index()
     {
         $businessPlans = BusinessPlan::all();
