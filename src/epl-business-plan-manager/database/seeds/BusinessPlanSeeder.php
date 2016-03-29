09cab85aa5b3267be1c61cc03844de9c75b41670
Cody Moorhouse <moorhousec2@mymacewan.ca>
2016-03-29 17:12:23 -0600

    Fix years in seeder

--- src/epl-business-plan-manager/database/seeds/BusinessPlanSeeder.php
+++ src/epl-business-plan-manager/database/seeds/BusinessPlanSeeder.php
@@ -14,16 +14,16 @@ class BusinessPlanSeeder extends Seeder
         DB::table('business_plans')->insert(array(
 
             array(
-                'start' => Carbon\Carbon::now(),
-                'end' => Carbon\Carbon::createFromDate(2014, 2, 5, 'America/Edmonton')),
+                'start' => Carbon\Carbon::createFromDate(2012, 1, 1, 'America/Edmonton'),
+                'end' => Carbon\Carbon::createFromDate(2014, 12, 31, 'America/Edmonton')),
 
             array(
-                'start' => Carbon\Carbon::now(),
+                'start' => Carbon\Carbon::createFromDate(2014, 2, 5, 'America/Edmonton'),
                 'end' => Carbon\Carbon::createFromDate(2016, 2, 5, 'America/Edmonton')),
 
             array(
-                'start' => Carbon\Carbon::now(),
-                'end' => Carbon\Carbon::createFromDate(2018, 2, 5, 'America/Edmonton')),
+                'start' => Carbon\Carbon::createFromDate(2016, 2, 5, 'America/Edmonton'),
+                'end' => Carbon\Carbon::createFromDate(2017, 2, 5, 'America/Edmonton')),
         ));
     }
 }
