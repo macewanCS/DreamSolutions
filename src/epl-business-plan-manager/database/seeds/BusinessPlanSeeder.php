bf633d2058fcf248e5b452eaa1bb94eb0f9a0f4d
Cody Moorhouse <moorhousec2@mymacewan.ca>
2016-03-29 20:21:09 -0600

    Change years, since future plans have no goals seeded

--- src/epl-business-plan-manager/database/seeds/BusinessPlanSeeder.php
+++ src/epl-business-plan-manager/database/seeds/BusinessPlanSeeder.php
@@ -14,16 +14,16 @@ class BusinessPlanSeeder extends Seeder
         DB::table('business_plans')->insert(array(
 
             array(
-                'start' => Carbon\Carbon::createFromDate(2012, 1, 1, 'America/Edmonton'),
-                'end' => Carbon\Carbon::createFromDate(2014, 12, 31, 'America/Edmonton')),
-
-            array(
-                'start' => Carbon\Carbon::createFromDate(2014, 2, 5, 'America/Edmonton'),
-                'end' => Carbon\Carbon::createFromDate(2016, 2, 5, 'America/Edmonton')),
+                'start' => Carbon\Carbon::createFromDate(2014, 1, 1, 'America/Edmonton'),
+                'end' => Carbon\Carbon::createFromDate(2016, 12, 31, 'America/Edmonton')),
 
             array(
                 'start' => Carbon\Carbon::createFromDate(2016, 2, 5, 'America/Edmonton'),
                 'end' => Carbon\Carbon::createFromDate(2018, 2, 5, 'America/Edmonton')),
+
+            array(
+                'start' => Carbon\Carbon::createFromDate(2018, 2, 5, 'America/Edmonton'),
+                'end' => Carbon\Carbon::createFromDate(2020, 2, 5, 'America/Edmonton')),
         ));
     }
 }
