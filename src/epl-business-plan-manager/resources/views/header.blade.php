8c3b13eb2a14f9257cfe7694533ecb460b15740a
Alan Yong <yonga@mymacewan.ca>
2016-03-29 18:34:23 -0600

    Add changelog link to header

--- src/epl-business-plan-manager/resources/views/header.blade.php
+++ src/epl-business-plan-manager/resources/views/header.blade.php
@@ -7,6 +7,7 @@
                 <ul>
                     <li><a href="/dashboard" class="{{ Request::segments()[0] == "dashboard" ? "active" : "" }}">Dashboard</a></li>
                     <li><a href="/view" class="{{ Request::segments()[0] == "view" ? "active" : "" }}">View Plan</a></li>
+                    <li><a href="/changes" class="{{ Request::segments()[0] == "changes" ? "active" : "" }}">Changelog  </a></li>
                     <li><a href="/manage" class="{{ Request::segments()[0] == "manage" ? "active" : "" }}">Manage Plan</a></li>
                     @if (Auth::user() && Auth::user()->is_admin)
                     <li><a href="/admin/users" class="{{ Request::segments()[0] == "admin" ? "active" : "" }}">Admin</a></li>
