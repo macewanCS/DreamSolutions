e28641fa32d41c6f92c04b29f316ef5dc358923e
Alan Yong <yonga@mymacewan.ca>
2016-03-29 18:21:17 -0600

    Prevent admin crash if not signed in

--- src/epl-business-plan-manager/app/Http/Middleware/RedirectIfNotAdmin.php
+++ src/epl-business-plan-manager/app/Http/Middleware/RedirectIfNotAdmin.php
@@ -15,7 +15,7 @@ class RedirectIfNotAdmin
      */
     public function handle($request, Closure $next)
     {
-        if ( !$request->user()->is_admin)
+        if ( !$request->user() || !$request->user()->is_admin)
         {
             return redirect('dashboard');
         }
