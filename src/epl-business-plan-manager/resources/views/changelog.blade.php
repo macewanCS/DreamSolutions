24e7825384da6461b9ff7f01b0b4405ef6a570be
Alan Yong <yonga@mymacewan.ca>
2016-03-30 21:02:56 -0600

    Implement exporting visible view plan

--- src/epl-business-plan-manager/resources/views/changelog.blade.php
+++ src/epl-business-plan-manager/resources/views/changelog.blade.php
@@ -7,7 +7,7 @@
 <script src='/js/jquery.dropdown.min.js'></script>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/js/select2.min.js"></script>
-<script src="js/changelog.js"></script>
+<script src="/js/changelog.js"></script>
 @stop
 
 @section('content')
--- src/epl-business-plan-manager/resources/views/view_plan.blade.php
+++ src/epl-business-plan-manager/resources/views/view_plan.blade.php
@@ -11,6 +11,7 @@
 <script src="/js/view.plan.js"></script>
 <script src="//cdn.rawgit.com/noelboss/featherlight/1.4.0/release/featherlight.min.js" type="text/javascript" charset="utf-8"></script>
 <link href="//cdn.rawgit.com/noelboss/featherlight/1.4.0/release/featherlight.min.css" type="text/css" rel="stylesheet" />
+<script src="/js/widget-output.js"></script>
 
 @stop
 
@@ -39,6 +40,7 @@
             <li><a href='#' data-jq-dropdown="#date-dropdown">Due date</a></li>
             <li><a href='#' data-jq-dropdown="#status-dropdown">Status</a></li>
         </ul>
+        {!! Form::button('Export CSV', ['id' => 'download']); !!}
         {!! Form::button('Reset View', ['id' => 'reset']); !!}
     </div>
     <table id="view-plan-table">
@@ -70,6 +72,7 @@
                     @endif
                     {{$goat->description}}
                     </td>
+                    <td class="hidden"></td>
                     <td align="right">
                     @if ($is_bplead || ($goat->goal_type == 'D' && in_array($goat->department_id, $leadOf) && $goat->type == 'O'))
                     <a href="/view/{{ $goat->id }}/edit" data-featherlight="ajax"><img src="/images/edit-white.png" width=15px height=15px></a>
